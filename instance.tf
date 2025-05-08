1f94108abeb62d6243cde50d6d7aaff5fe1c70b9
meeeeeeeee <me@you.org>
2025-05-08 19:59:29 +0000

    test

--- instance.tf
+++ instance.tf
@@ -15,13 +15,13 @@ resource "aws_instance" "wordpress" {
 
 resource "aws_instance" "mysql" {
   ami                         = "ami-0f88e80871fd81e91"
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet1.id
   key_name                    = aws_key_pair.deployer.key_name
   vpc_security_group_ids      = [aws_security_group.secure2.id]
   associate_public_ip_address = true
   user_data                   = file("install_mysql.sh")
   tags = {
-    Name = "Mysql"
+    Name = "Mysql-v3"
   }
 }
