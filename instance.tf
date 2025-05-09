9182087333b02ec8d3e4b51f3e76e9dd5c64327f
meeeeeeeee <me@you.org>
2025-05-09 11:43:22 +0000

    test GH hosted runner v19

--- instance.tf
+++ instance.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_instance" "wordpress" {
   ami                         = "ami-0f88e80871fd81e91"
   instance_type               = "t2.micro"
@@ -25,3 +26,4 @@ resource "aws_instance" "mysql" {
     Name = "Mysql-bv-v5"
   }
 }
+*/
