cf7718156b05dd530f09c3f903b3aeb52b27022d
meeeeeeeee <me@you.org>
2025-05-19 18:16:35 +0000

    tst doc update v8

--- guff.tf
+++ guff.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "terraform-example-queue"
   delay_seconds             = 90
@@ -10,9 +10,18 @@ resource "aws_sqs_queue" "terraform_queue" {
     Environment = "production"
   }
 }
-*/
+
 resource "aws_s3_bucket" "example2" {
   bucket = "my-tf-test-bucket-022566422092"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "example3" {
+  bucket = "my-fdgggdgdg022566422092"
 
   tags = {
     Name        = "My bucket"
