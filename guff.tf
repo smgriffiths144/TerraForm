fc7b992b474914484de4c606c22072c12681d968
meeeeeeeee <me@you.org>
2025-05-20 10:12:13 +0000

    module test v2

--- guff.tf
+++ guff.tf
@@ -1,51 +1,4 @@
 
-resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "terraform-example-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-
-  tags = {
-    Environment = "production"
-  }
-}
-
-resource "aws_s3_bucket" "example1" {
-  bucket = "my1-tf-test-bucket-022566422092"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "example2" {
-  bucket = "my2-fdgggdgdg022566422092"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "example3" {
-  bucket = "my3-tf-tesvvvcvt-bucket-022566422092"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "example4" {
-  bucket = "4vvvdvvdvggdgdg022566422092"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
@@ -58,5 +11,17 @@ module "s3_bucket" {
 
   versioning = {
     enabled = true
+  }
+}
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+
+  name = "fifo"
+
+  fifo_queue = true
+
+  tags = {
+    Environment = "dev"
   }
 }
