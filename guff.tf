0903cb61e50a67d1b6478fd225a8bd6349529239
meeeeeeeee <me@you.org>
2025-05-20 11:09:42 +0000

    module test v6

--- guff.tf
+++ guff.tf
@@ -4,7 +4,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucketg022566422092"
-  acl    = "private"
+  #acl    = "private"
 
   #control_object_ownership = true
   #object_ownership         = "ObjectWriter"
