98bb5cf704678fbee6f08b53ca2dabd7ef3adcc8
meeeeeeeee <me@you.org>
2025-05-20 20:16:33 +0000

    deploying a website v1

--- instance.tf
+++ instance.tf
@@ -1,19 +1,19 @@
-/*
-resource "aws_instance" "wordpress" {
-  ami                         = "ami-0f88e80871fd81e91"
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.subnet1.id
+
+resource "aws_instance" "webserevr" {
+  ami           = "ami-0f88e80871fd81e91"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.subnet1.id
   #key_name                    = aws_key_pair.deployer.key_name
   vpc_security_group_ids      = [aws_security_group.secure1.id]
   associate_public_ip_address = true
   depends_on                  = [aws_internet_gateway.intgw]
-  user_data                   = file("install_wordpress.sh")
+  user_data                   = file("install_httpd.sh")
   tags = {
     Name = "Wordpress-v6"
     Guff = "Stuff"
   }
 }
-
+/*
 resource "aws_instance" "mysql" {
   ami                         = "ami-0f88e80871fd81e91"
   instance_type               = "t2.micro"
