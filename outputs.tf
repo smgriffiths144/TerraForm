ef3b1a480ea8dbbb33a20a143a6c09350709fe76
meeeeeeeee <me@you.org>
2025-05-20 18:48:22 +0000

    mod v14

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "secuirty_group_arn" {
   value = aws_security_group.secure1.arn // via local or just
   #value = data.aws_caller_identity.current.account_id
 }
+
+output "cloudwatch_log_group_name" {
+  description = "Name (id) of the cloudwatch log group"
+  value       = module.s3_bucket_remote_module.cloudwatch_log_group_name
+}
