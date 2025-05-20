b98de339b7bae00464968d605f5404677619aecf
meeeeeeeee <me@you.org>
2025-05-20 19:59:36 +0000

    pre commit hooks v7

--- guff.tf
+++ guff.tf
@@ -4,7 +4,7 @@ module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.7.0"
 
-  bucket = "my-s3-bucketg022566422092"
+  bucket = "my-s3-buc44444ketg022566422092"
   #acl    = "private"
 
   #control_object_ownership = true
@@ -18,7 +18,7 @@ module "s3_bucket" {
 module "sqs" {
   source = "terraform-aws-modules/sqs/aws"
 
-  name = "fifo"
+  name = "fiforrrrrrrer"
 
   fifo_queue = true
 
@@ -30,7 +30,7 @@ module "sqs" {
 module "website_s3_bucket" {
   source = "./modules/s3"
 
-  bucket_name = "022566422092-porthmadoggo"
+  bucket_name = "022566422092-porthmadoggo3434"
 
   tags = {
     Terraform   = "true"
@@ -58,7 +58,7 @@ module "website_s3_bucket_v3" {
 
   tags = {
     Terraform   = "true"
-    Environment = "devvvvvyyyyyy"
+    Environment = "devvvvvyyyyyyvvv"
     Module      = "local"
   }
 }
@@ -66,16 +66,16 @@ module "website_s3_bucket_v3" {
 module "s3_bucket_remote_module" {
   source = "git::https://github.com/smgriffiths144/terraform-modules.git" #?ref=v1.6.0"
 
-  bucket_name       = "022566422092-test-remote-module"
-  q_name            = "remoteqname"
-  q2_name           = "remoteq2name"
-  name              = "cwlog-test"
-  retention_in_days = "1"
+  bucket_name = "022566422092-test-remote-module"
+  q_name      = "remoteqname"
+  #q2_name           = "remoteq2nam"
+  #name              = "cwlog-test"
+  #retention_in_days = "1"
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
-    Module      = "remote"
+    Terraform   = "truevvvv"
+    Environment = "devvvvv"
+    Module      = "remotevvvv"
 
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,9 @@ output "secuirty_group_arn" {
   value = aws_security_group.secure1.arn // via local or just
   #value = data.aws_caller_identity.current.account_id
 }
-
+/*
 output "cloudwatch_log_group_name" {
   description = "Name (id) of the cloudwatch log group"
   value       = module.s3_bucket_remote_module.cloudwatch_log_group_name
 }
+*/
