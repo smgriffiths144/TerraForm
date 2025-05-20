31a43d9cc3edea673195819bd3b00acbbcff7ca2
meeeeeeeee <me@you.org>
2025-05-20 17:55:01 +0000

    mod v11

--- guff.tf
+++ guff.tf
@@ -1,7 +1,8 @@
 
 
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws?ref=v4.7.0"
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.7.0"
 
   bucket = "my-s3-bucketg022566422092"
   #acl    = "private"
