d6e76585af30dfe328db00f992c9727cc48fd7c5
meeeeeeeee <me@you.org>
2025-05-21 13:02:47 +0000

    web v6

--- instance.tf
+++ instance.tf
@@ -24,7 +24,22 @@ resource "aws_lb" "test" {
   }
 }
 
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
 
+resource "aws_lb_listener" "test" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
+}
 
 
 resource "aws_iam_instance_profile" "ec2_profile" {
