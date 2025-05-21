891ae4b67eb90c3a28483480143d210c2ef47f28
meeeeeeeee <me@you.org>
2025-05-21 12:52:16 +0000

    web v5

--- vpc.tf
+++ vpc.tf
@@ -54,7 +54,7 @@ resource "aws_subnet" "subnet1" {
 
 resource "aws_subnet" "subnet3" {
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-1b"
   cidr_block              = "10.24.3.0/24"
   map_public_ip_on_launch = "true"
   tags = {
