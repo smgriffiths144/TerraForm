c5ff1109e42e93eec9317e86af8e4a0fcd8f7dc5
Stefan Griffiths <gristef+testuser5@amazon.co.uk>
2025-05-22 17:21:27 +0100

    add noed exporter v2

--- install_httpd.sh
+++ install_httpd.sh
@@ -2,7 +2,7 @@
 yum install httpd -y
 service httpd start
 chkconfig httpd on
-echo 'guff and stuff' > /var/www/html/index.html
+echo 'guff and stuff v3' > /var/www/html/index.html
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.9.1.linux-amd64.tar.gz
