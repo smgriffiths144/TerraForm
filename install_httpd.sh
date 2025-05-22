8d0a9aa08435600f29f2dac26c6c8193d43f7fa4
Stefan Griffiths <gristef+testuser5@amazon.co.uk>
2025-05-22 17:18:33 +0100

    add noed exporter v1

--- install_httpd.sh
+++ install_httpd.sh
@@ -4,3 +4,7 @@ service httpd start
 chkconfig httpd on
 echo 'guff and stuff' > /var/www/html/index.html
 
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.9.1.linux-amd64.tar.gz
+cd node_exporter-1.9.1.linux-amd64
+./node_exporter
